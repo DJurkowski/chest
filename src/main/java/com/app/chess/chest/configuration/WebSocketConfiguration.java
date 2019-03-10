ac8fad9f72a96c8be0fd4c70b16f294b66ef0287
Dominik Jurkowski <jurkowski.dominik.andrzej@gmail.com>
2019-03-10 23:18:19 +0100

    Refactor webSocket policy. Change for one socket per user

--- src/main/java/com/app/chess/chest/configuration/WebSocketConfiguration.java
+++ src/main/java/com/app/chess/chest/configuration/WebSocketConfiguration.java
@@ -18,6 +18,6 @@ public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfig
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/api")
-                .enableSimpleBroker("/privateRoom", "/gameRoom", "/notification");
+                .enableSimpleBroker("/privateRoom", "/gameRoom", "/notification", "/privateMessage");
     }
 }
--- src/main/java/com/app/chess/chest/controller/WebSocketController.java
+++ src/main/java/com/app/chess/chest/controller/WebSocketController.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
@@ -22,6 +23,8 @@ public class WebSocketController {
     private final SimpMessagingTemplate template;
     private final RoomService roomService;
     private final UserDetailsServiceImpl userService;
+
+    private String[] messageTab = null;
 
     @Autowired
     public WebSocketController(SimpMessagingTemplate template, RoomService roomService, UserDetailsServiceImpl userService) {
@@ -47,6 +50,30 @@ public class WebSocketController {
     @MessageMapping("/notifi/{userToId}")
     public void sendNotificationMessage(@NotNull String message, @DestinationVariable Long userToId) throws IOException{
         this.template.convertAndSend("/notification/" + userService.getUsername(userToId), message);
+
+    }
+
+    @MessageMapping("/websocket/{userToId}")
+    public void sendWebSocketMessage(@NotNull String message, @DestinationVariable String userToId) throws IOException{
+        String messageOut = "";
+        messageTab = message.split(";", 5);
+        System.out.println("Jestem MEssage Tab" + messageTab[0] );
+        switch(messageTab[0]){
+            case "chat":
+                String messageDisplay = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+ " - " +" @" + messageTab[2] + " : " + messageTab[4];
+                messageOut = messageTab[0] + ";" +  messageTab[1] + ";" + messageTab[2] + ";" + messageTab[3] + ";" + messageDisplay;
+                this.template.convertAndSend("/privateMessage/" + messageTab[3], messageOut);
+                roomService.addMessageToMessageList( messageDisplay, roomService.getRoomId(messageTab[1]));
+                System.out.println("message: "+ messageDisplay + " room: "+ messageTab[1]);
+                break;
+            case "game":
+//                dopisac logike do gry
+                break;
+            case "noti":
+                messageOut = messageTab[0] + ";" +  messageTab[1] + ";" + messageTab[2] + ";" + userService.getUsername(Long.parseLong(messageTab[3])) + ";" + messageTab[4];
+                this.template.convertAndSend("/privateMessage/" + userService.getUsername(Long.parseLong(messageTab[3])), messageOut);
+                break;
+        }
 
     }
 }
